9d6b22d7ce836131821158d9bf448df2faffabf3
YuanSamuel <samuel6161@gmail.com>
2019-02-16 22:40:55 -0600

    Basics

--- sketch.js
+++ sketch.js
@@ -1,18 +1,12 @@
-let avenir;
-function preload() {
-  avenir = loadFont('assets/Avenir.otf');
-}
+
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
 	textSize(50);
 	textAlign(CENTER, CENTER);
-	textFont(avenir);
 }
 
 function draw() {
 	background(220, 0, 220);
-	let time = millis();
-	rotateZ(time / 1234);
 	text("Welcome to my website!!", windowWidth / 2, windowHeight / 2);
 	ellipse(mouseX, mouseY, 80, 80);
 }
