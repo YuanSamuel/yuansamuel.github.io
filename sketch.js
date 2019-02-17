d8fc116432513356df9c9ddd0b27fa5623ef7065
YuanSamuel <samuel6161@gmail.com>
2019-02-16 22:20:12 -0600

    More testing

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	textSize(50);
+	textAlign(CENTER, CENTER);
+
 }
 
 function draw() {
 	background(220, 0, 220);
-	textSize(50);
-	textAlign(CENTER, CENTER)
-	text("Welcome to my website!!", windowWidth / 2, windowHeight / 2);
 	let time = millis();
 	rotateZ(time / 1234);
+	text("Welcome to my website!!", windowWidth / 2, windowHeight / 2);
 	ellipse(mouseX, mouseY, 80, 80);
 }
