bbcbae541e4b023c8aaaee94a2c598e3a5a35392
YuanSamuel <samuel6161@gmail.com>
2019-02-16 22:44:40 -0600

    Circle goes behind the text

--- sketch.js
+++ sketch.js
@@ -7,6 +7,6 @@ function setup() {
 
 function draw() {
 	background(220, 0, 220);
-	text("Welcome to my website!!", windowWidth / 2, windowHeight / 2);
 	ellipse(mouseX, mouseY, 80, 80);
+	text("Welcome to my website!!", windowWidth / 2, windowHeight / 2);
 }
