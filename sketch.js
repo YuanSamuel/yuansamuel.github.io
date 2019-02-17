93cd22f2fb3fad6e7391a4015d8ac56c5a77c215
YuanSamuel <samuel6161@gmail.com>
2019-02-16 22:17:46 -0600

    Messing with text

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ function setup() {
 
 function draw() {
 	background(220, 0, 220);
+	textSize(50);
+	textAlign(CENTER, CENTER)
 	text("Welcome to my website!!", windowWidth / 2, windowHeight / 2);
+	let time = millis();
+	rotateZ(time / 1234);
 	ellipse(mouseX, mouseY, 80, 80);
 }
